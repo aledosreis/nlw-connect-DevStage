73d6332cd25e96eb4ecdc6e100fff03c195b382d
Alessandro Reis <alessandrodreis23@gmail.com>
2025-02-20 08:48:06 -0300

    setup fastify cors

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -1,6 +1,9 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 
 const app = fastify();
+
+app.register(fastifyCors); // Em produção precisa especificar a URL do front end
 
 app.get("/hello", () => {
   return "Hello World";
