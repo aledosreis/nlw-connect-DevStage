72e280cd54d25e1db09aeb73dea5312e909df56b
Alessandro Reis <alessandrodreis23@gmail.com>
2025-02-20 09:08:50 -0300

    add example route with validation and serialization

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -3,18 +3,44 @@ import { fastifyCors } from "@fastify/cors";
 import {
   validatorCompiler,
   serializerCompiler,
+  ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import { z } from "zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors); // Em produção precisa especificar a URL do front end
 
-app.get("/hello", () => {
-  return "Hello World";
-});
+app.post(
+  "/subscriptions",
+  {
+    schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+      }),
+      response: {
+        201: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+      },
+    },
+  },
+  async (request, reply) => {
+    const { name, email } = request.body;
+
+    // Criação da inscrição no banco de dados
+
+    return reply.status(201).send({
+      name,
+      email,
+    });
+  }
+);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
